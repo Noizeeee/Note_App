b240117bf02209303b53cf30167f146c70d71a3d
raine <raineaveryocampo@gmail.com>
2026-06-26 16:03:20 +0800

    fix: notes serializer

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -15,7 +15,8 @@ from datetime import timedelta
 import os
 from dotenv import load_dotenv
 
-load_dotenv()
+BASE_DIR = Path(__file__).resolve().parent.parent  # ← must be first
+load_dotenv(BASE_DIR / '.env')    
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,9 +26,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-##zt3f$qb@7!ya#st(g4m3j9qfhq*vp^&=lcb5i^=x(xd5&zud'
 
-SECRET_KEY = os.getenv('django-insecure-##zt3f$qb@7!ya#st(g4m3j9qfhq*vp^&=lcb5i^=x(xd5&zud')
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
--- backend/users/urls.py
+++ backend/users/urls.py
@@ -3,7 +3,7 @@ from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from .views import RegisterView
 
 urlpatterns = [
-    path('register/', RegisterView.as_view(), method='POST'),
+    path('register/', RegisterView.as_view()),
     path('login/', TokenObtainPairView.as_view()),
     path('refresh/', TokenRefreshView.as_view()),
 ]
